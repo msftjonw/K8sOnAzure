4a3c09fbf1e4afc68e77bf7c11a7440c58be6745
msftjonw <35560783+msftjonw@users.noreply.github.com>
2022-08-24 21:26:33 -0700

    Update installK8sRequiredComponents.sh

--- installK8sRequiredComponents.sh
+++ installK8sRequiredComponents.sh
@@ -37,7 +37,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - >> 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >> /var/log/k8s_install_output.txt 2>> /var/log/k8s_install_errors.txt
 sudo apt update && sudo apt install containerd.io -y >> /var/log/k8s_install_output.txt 2>> /var/log/k8s_install_errors.txt
 sudo mkdir -p /etc/containerd >> /var/log/k8s_install_output.txt 2>> /var/log/k8s_install_errors.txt
-sudo containerd config default>/etc/containerd/config.toml && exit >> /var/log/k8s_install_output.txt 2>> /var/log/k8s_install_errors.txt
+#sudo containerd config default>/etc/containerd/config.toml && exit >> /var/log/k8s_install_output.txt 2>> /var/log/k8s_install_errors.txt
 sudo systemctl restart containerd && sudo systemctl enable containerd >> /var/log/k8s_install_output.txt 2>> /var/log/k8s_install_errors.txt
 
 #Install kubeadm kubelet and kubectl
